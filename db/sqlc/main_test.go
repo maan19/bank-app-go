53a0e62671d69686ca56fdcf447ff51135d67042
maan jagdeep <maanj872@gmail.com>
2023-02-07 12:57:14 -0800

    added-tx-except-balances

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -15,12 +15,14 @@ const (
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	db, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal(err)
 	}
-	testQueries = New(db)
+	testQueries = New(testDB)
 	os.Exit(m.Run())
 }
