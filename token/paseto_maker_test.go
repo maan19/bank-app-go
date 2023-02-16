bbe76a1dc69a4cf4d4dd881d4a1981920c9f3486
maan jagdeep <maanj872@gmail.com>
2023-02-16 02:54:23 -0800

    added-paseto-token-code+tests

---
+++ token/paseto_maker_test.go
@@ -0,0 +1,51 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/maan19/bank-app-go/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPasetoMaker(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomString(32)
+	duration := time.Minute
+	issuedAt := time.Now()
+	expiredAt := time.Now().Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+}
+
+func TestPasetoMakerExpiredToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	username := util.RandomString(32)
+	duration := -time.Minute
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.Nil(t, payload)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+}
+
+// TODO: add test for InvalidToken
